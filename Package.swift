f9db7be20e60b662f032dd8c1e9d64ef3e86a0d9
Casper Rasmussen <cr@nodes.dk>
2016-12-29 13:00:22 +0100

    added Creator+mysqltypes

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "Sugar"
+    name: "Sugar",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1)
+    ]
 )
