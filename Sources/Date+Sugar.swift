1c13400dfbe54e401d73c6963375351c94016799
Casper Rasmussen <cr@nodes.dk>
2017-01-20 15:38:45 +0100

    Added end of day and start of day

--- Sources/Date+Sugar.swift
+++ Sources/Date+Sugar.swift
@@ -1,6 +1,20 @@
 import Foundation
 
 extension Date {
+      var startOfDay : Date {
+        let calendar = Calendar.current
+        let unitFlags = Set<Calendar.Component>([.year, .month, .day])
+        let components = calendar.dateComponents(unitFlags, from: self)
+        return calendar.date(from: components)!
+    }
+    
+    var endOfDay : Date {
+        var components = DateComponents()
+        components.day = 1
+        let date = Calendar.current.date(byAdding: components, to: self.startOfDay)
+        return (date?.addingTimeInterval(-1))!
+    }
+    
     
     public enum Error: Swift.Error {
         case couldNotParse
