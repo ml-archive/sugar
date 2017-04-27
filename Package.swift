81b90b89452399a39b7cd23db25e3e364bab2417
Valentin Corral <valencg90@gmail.com>
2017-04-27 09:39:02 +0200

    Comments changes

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ let package = Package(
     name: "Sugar",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
-        .Package(url: "https://github.com/vapor/fluent-provider.git", Version(1,0,0, prereleaseIdentifiers: ["beta"]))//,
-        //.Package(url: "https://github.com/bygri/vapor-forms.git", majorVersion:0)
+        .Package(url: "https://github.com/vapor/fluent-provider.git", Version(1,0,0, prereleaseIdentifiers: ["beta"]))
     ]
 )
