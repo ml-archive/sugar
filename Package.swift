d6c16301ef891b00875cc6f96236a0f5fd99570b
Steffen D. Sommer <stso@nodes.dk>
2017-05-22 11:10:17 +0200

    Made it compile + definable min length

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "Sugar",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1)
+        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/validation.git", majorVersion: 1),
     ]
 )
