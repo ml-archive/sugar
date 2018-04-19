dabcaabdc7e4a4ead42f84ee4a8bfb428b065533
Steffen D. Sommer <steffendsommer@icloud.com>
2018-04-19 15:36:19 +0200

    Add hashed password

---
+++ Sources/Sugar/Authentication/HashedPassword.swift
@@ -0,0 +1,32 @@
+import Crypto
+
+public protocol HasHashedPassword {
+    static var bCryptCost: Int { get }
+}
+
+public struct HashedPassword: Codable, Equatable {
+    public let value: String
+    fileprivate init(_ input: LosslessStringConvertible) {
+        value = input.description
+    }
+}
+
+extension HasHashedPassword {
+    public static func hashPassword(_ data: LosslessDataConvertible) throws -> HashedPassword {
+        return try HashedPassword(BCrypt.hash(data, cost: bCryptCost).base64EncodedString())
+    }
+}
+
+// MARK: - MySQLDataConvertible
+
+import MySQL
+
+extension HashedPassword: MySQLDataConvertible {
+    public func convertToMySQLData() throws -> MySQLData {
+        return MySQLData(string: value)
+    }
+
+    public static func convertFromMySQLData(_ mysqlData: MySQLData) throws -> HashedPassword {
+        return try self.init(mysqlData.decode(String.self))
+    }
+}
