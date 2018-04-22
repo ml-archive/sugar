2b22db012e30ffac1a899ff4cd9ca2442365276d
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-04-22 18:10:50 +0200

    Verify password strength before hashing

--- Sources/Sugar/Authentication/HashedPassword.swift
+++ Sources/Sugar/Authentication/HashedPassword.swift
@@ -1,10 +1,7 @@
 import Crypto
 
-public protocol HasHashedPassword {
-    static var bCryptCost: Int { get }
-    var password: HashedPassword { get }
-}
-
+/// Wrapper for password strings that can only be instantiated using hashed passwords of sufficient
+/// strength.
 public struct HashedPassword: Codable, Equatable {
     public let value: String
     fileprivate init(_ input: LosslessStringConvertible) {
@@ -12,14 +9,46 @@ public struct HashedPassword: Codable, Equatable {
     }
 }
 
+public protocol HasHashedPassword {
+    static var bCryptCost: Int { get }
+    static func validateStrength(ofPassword password: String) throws
+    var password: HashedPassword { get }
+}
+
 extension HasHashedPassword {
-    public static func hashPassword(_ data: LosslessDataConvertible) throws -> HashedPassword {
-        return try HashedPassword(BCrypt.hash(data, cost: bCryptCost))
+
+    /// Hashes password.
+    ///
+    /// - Parameter password: password to validate and hash
+    /// - Returns: a HashedPassword value based on the input and `bCryptCost`
+    /// - Throws: PasswordError.weakPassword when password is not strong enough
+    public static func hashPassword(_ password: String) throws -> HashedPassword {
+        try validateStrength(ofPassword: password)
+        return try HashedPassword(BCrypt.hash(password, cost: bCryptCost))
     }
 
+    /// Validates password strength.
+    ///
+    /// - Parameter password: password string to validate
+    /// - Throws: PasswordError.weakPassword when password is not strong enough
+    public static func validateStrength(ofPassword password: String) throws {
+        // TODO: stricter validation
+        guard password.count > 8 else {
+            throw PasswordError.weakPassword
+        }
+    }
+
+    /// Verifies whether provided password matches the hashed password.
+    ///
+    /// - Parameter password: password to verify
+    /// - Returns: a boolean indicating whether passwords match
     public func verify(_ password: String) throws -> Bool {
         return try BCrypt.verify(password, created: self.password.value)
     }
+}
+
+public enum PasswordError: Error {
+    case weakPassword
 }
 
 // MARK: - MySQLDataConvertible
