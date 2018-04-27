5cb859e6e7b6adeb072016a726caf2f02a67276c
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-04-27 10:41:54 +0200

    Use correct auth

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/vapor/auth.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
         .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
     ],
