5820aae13cc74e2e1d57030f3bbb0be08d0b8b2c
Steffen D. Sommer <steffendsommer@icloud.com>
2018-05-02 10:51:48 +0200

    … it is required

--- Package.swift
+++ Package.swift
@@ -17,13 +17,14 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Sugar",
-            dependencies: ["Authentication", "Fluent", "Vapor", "Leaf"]),
+            dependencies: ["Authentication", "Fluent", "Vapor", "Leaf", "JWT"]),
         .testTarget(
             name: "SugarTests",
             dependencies: ["Sugar"]),
