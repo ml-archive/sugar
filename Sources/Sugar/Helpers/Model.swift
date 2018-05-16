9dc07c80a9196713b5a2be82b3c4547a1658d9a0
Steffen D. Sommer <steffendsommer@icloud.com>
2018-05-16 15:32:04 +0200

    Add Model helper

---
+++ Sources/Sugar/Helpers/Model.swift
@@ -0,0 +1,10 @@
+public extension Model where Database: QuerySupporting {
+    public static func requireFind(
+        _ id: ID,
+        on worker: DatabaseConnectable
+    ) throws -> Future<Self> {
+        return try Self
+            .find(id, on: worker)
+            .unwrap(or: Abort(.notFound, reason: "\(Self.self) with id \(id) not found"))
+    }
+}
