088760a4a81af3ff2cef0d4b879700a0f68184a0
Steffen D. Sommer <steffendsommer@icloud.com>
2018-06-06 15:49:27 +0200

    Add saveOrModify

--- Sources/Sugar/Helpers/Model.swift
+++ Sources/Sugar/Helpers/Model.swift
@@ -10,4 +10,25 @@ public extension Model where Database: QuerySupporting {
             .find(id, on: worker)
             .unwrap(or: Abort(.notFound, reason: "\(Self.self) with id \(id) not found"))
     }
+
+    public func saveOrUpdate<T>(
+        given values: [WritableKeyPath<Self, T>: T],
+        on db: DatabaseConnectable
+    ) throws -> Future<Self> {
+        var query = Self.query(on: db)
+        for (field, value) in values {
+            query = try query.filter(field == value)
+        }
+
+        return query.first().flatMap(to: Self.self) { result in
+            guard let result = result else {
+                return self.save(on: db)
+            }
+
+            var copy = self
+            copy.fluentID = result.fluentID
+
+            return copy.save(on: db)
+        }
+    }
 }
