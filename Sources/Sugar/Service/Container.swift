d1ef50dafbbd78fab29b2d3eaf9fc8022bb93643
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-06-17 08:26:28 +0200

    Remove baseURLString
    This did not work well because it only uses the local address, not the
    actual URL.

--- Sources/Sugar/Service/Container.swift
+++
@@ -1,8 +0,0 @@
-import Vapor
-
-extension Container {
-    public func baseURLString(includePort: Bool = true) throws -> String {
-        let config = try make(NIOServerConfig.self)
-        return "http://\(config.hostname)" + (includePort ? ":\(config.port)" : "")
-    }
-}
