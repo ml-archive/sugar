29e3c3fb551fddee910c151859c0b03923258e95
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-06-18 10:02:46 +0200

    Update for proposed leaf change

--- Sources/Sugar/Helpers/LeafTagConfig.swift
+++ Sources/Sugar/Helpers/LeafTagConfig.swift
@@ -1,7 +1,7 @@
 import Leaf
 
 public extension LeafTagConfig {
-    public mutating func use(_ list: [String: TagRenderer]) {
+    public func use(_ list: [String: TagRenderer]) {
         for item in list {
             self.use(item.value, as: item.key)
         }
