35c95f80814a4b9409942340bf03c79ab1e1c430
cweinberger <christian.weinberger@me.com>
2018-07-03 13:38:34 +0200

    Make class and its methods public

--- Sources/Sugar/Middlewares/SimpleAuthMiddleware.swift
+++ Sources/Sugar/Middlewares/SimpleAuthMiddleware.swift
@@ -2,21 +2,21 @@ import HTTP
 import Vapor
 import AuthProvider
 
-final class SimpleAuthMiddleware: Middleware {
+public final class SimpleAuthMiddleware: Middleware {
     private let token: String
 
-    internal init(token: String) {
+    public init(token: String) {
         self.token = token
     }
 
-    func respond(to request: Request, chainingTo next: Responder) throws -> Response {
+    public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
         let resp = try next.respond(to: request)
 
         guard
             let header = request.auth.header,
             header.string == token
-        else {
-            throw Abort.unauthorized
+            else {
+                throw Abort.unauthorized
         }
 
         return resp
