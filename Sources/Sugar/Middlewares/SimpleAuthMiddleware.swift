7f9e8ca7cd85bf91c561b2484eab393a26c97e67
cweinberger <christian.weinberger@me.com>
2018-07-03 13:04:04 +0200

    Add simple auth middleware (API KEY)

---
+++ Sources/Sugar/Middlewares/SimpleAuthMiddleware.swift
@@ -0,0 +1,22 @@
+import HTTP
+
+internal final class SimpleAuthMiddleware: Middleware {
+    private let token: String
+
+    internal init(token: String) {
+        self.token = token
+    }
+
+    func respond(to request: Request, chainingTo next: Responder) throws -> Response {
+        let resp = try next.respond(to: request)
+
+        guard
+            let header = request.auth.header,
+            header.string == token
+        else {
+            throw Abort.unauthorized
+        }
+
+        return resp
+    }
+}
