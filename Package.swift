10c0767a3042891c9e172e9dd9cd63e0107a8d80
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-09-17 12:41:12 +0200

    Update dependency and clean up Package.swift

--- Package.swift
+++ Package.swift
@@ -6,28 +6,27 @@ import PackageDescription
 let package = Package(
     name: "Sugar",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Sugar",
-            targets: ["Sugar"]),
+            targets: ["Sugar"]
+        ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.1"),
         .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/redis.git", from: "3.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Sugar",
-            dependencies: ["Authentication", "Fluent", "Vapor", "Leaf", "JWT", "Redis"]),
+            dependencies: ["Authentication", "Fluent", "Vapor", "Leaf", "JWT", "Redis"]
+        ),
         .testTarget(
             name: "SugarTests",
-            dependencies: ["Sugar"]),
+            dependencies: ["Sugar"]
+        ),
     ]
 )
