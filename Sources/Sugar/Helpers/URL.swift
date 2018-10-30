950f31273f1dd96056d696554ff092e8570c9154
Steffen D. Sommer <steffendsommer@icloud.com>
2018-10-30 11:19:25 +0100

    Add query helper on URL

---
+++ Sources/Sugar/Helpers/URL.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+public extension URL {
+    public func addQueryItems(_ item: [URLQueryItem]) -> URL? {
+        guard var components = URLComponents(url: self, resolvingAgainstBaseURL: false) else {
+            return nil
+        }
+
+        let currentItems = components.queryItems ?? []
+        components.queryItems = currentItems + item
+        return components.url
+    }
+
+    public func addQueryItems(from URL: URL) -> URL? {
+        guard
+            let items = URLComponents(url: URL, resolvingAgainstBaseURL: false)?.queryItems
+        else {
+            return nil
+        }
+
+        return addQueryItems(items)
+    }
+}
