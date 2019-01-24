d19e06a2167b00fd81c3f03dcd57a18e98abf453
Steffen D. Sommer <steffendsommer@icloud.com>
2019-01-24 20:21:00 +0100

    Add support for conditional auth

--- Sources/Sugar/Middlewares/SimpleAuthMiddleware.swift
+++ Sources/Sugar/Middlewares/SimpleAuthMiddleware.swift
@@ -4,17 +4,21 @@ import AuthProvider
 
 public final class SimpleAuthMiddleware: Middleware {
     private let token: String
+    private let shouldAuth: (Request) -> Bool
 
-    public init(token: String) {
+    public init(token: String, shouldAuth: @escaping (Request) -> Bool = { _ in true }) {
         self.token = token
+        self.shouldAuth = shouldAuth
     }
 
     public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
+        // Make sure we should auth
+        guard shouldAuth(request) else {
+            return try next.respond(to: request)
+        }
 
-        guard
-            let header = request.auth.header,
-            header.string == token
-        else {
+        // Check for token
+        guard let header = request.auth.header, header.string == token else {
             throw Abort.unauthorized
         }
 
